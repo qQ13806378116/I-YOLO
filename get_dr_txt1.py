10177da1c6b057dc92c07133fd96c8b6ec26304b
qQ13806378116 <70269810+qQ13806378116@users.noreply.github.com>
2022-03-08 08:53:06 +0800

    Add files via upload

---
+++ get_dr_txt1.py
@@ -0,0 +1,110 @@
+
+from yolo import YOLO
+from PIL import Image
+from keras.layers import Input
+from keras.applications.imagenet_utils import preprocess_input
+from keras import backend as K
+from utils.utils import letterbox_image
+from nets.yolo3 import yolo_body,yolo_eval
+from tqdm import tqdm
+import colorsys
+import numpy as np
+import os
+
+import tensorflow as tf
+from tensorflow.compat.v1 import ConfigProto, InteractiveSession
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+class mAP_YOLO(YOLO):
+
+    def generate(self):
+        self.score = 0.01
+        model_path = os.path.expanduser(self.model_path)
+        assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
+        
+        
+        num_anchors = len(self.anchors)
+        num_classes = len(self.class_names)
+
+   
+        try:
+            self.yolo_model = load_model(model_path, compile=False)
+        except:
+            self.yolo_model = yolo_body(Input(shape=(None,None,3)), num_anchors//3, num_classes, phi = self.phi)
+            self.yolo_model.load_weights(self.model_path)
+        else:
+            assert self.yolo_model.layers[-1].output_shape[-1] == \
+                num_anchors/len(self.yolo_model.output) * (num_classes + 5), \
+                'Mismatch between model and given anchor and class sizes'
+
+        print('{} model, anchors, and classes loaded.'.format(model_path))
+
+        # 画框设置不同的颜色
+        hsv_tuples = [(x / len(self.class_names), 1., 1.)
+                      for x in range(len(self.class_names))]
+        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+        self.colors = list(
+            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+                self.colors))
+
+        # 打乱颜色
+        np.random.seed(10101)
+        np.random.shuffle(self.colors)
+        np.random.seed(None)
+
+        self.input_image_shape = K.placeholder(shape=(2, ))
+
+        boxes, scores, classes = yolo_eval(self.yolo_model.output, self.anchors,
+                num_classes, self.input_image_shape,
+                score_threshold=self.score, iou_threshold=self.iou)
+        return boxes, scores, classes
+
+
+    def detect_image(self, image_id, image):
+        f = open("./input/detection-results/"+image_id+".txt","w") 
+        
+        boxed_image = letterbox_image(image, self.model_image_size)
+        image_data = np.array(boxed_image, dtype='float32')
+        image_data /= 255.
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+     
+        out_boxes, out_scores, out_classes = self.sess.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
+                K.learning_phase(): 0
+            })
+
+        for i, c in enumerate(out_classes):
+            predicted_class = self.class_names[int(c)]
+            score = str(out_scores[i])
+
+            top, left, bottom, right = out_boxes[i]
+            f.write("%s %s %s %s %s %s\n" % (predicted_class, score[:6], str(int(left)), str(int(top)), str(int(right)),str(int(bottom))))
+
+        f.close()
+        return 
+
+yolo = mAP_YOLO()
+
+image_ids = open('VOCdevkit/VOC2007/ImageSets/Main/test.txt').read().strip().split()
+
+if not os.path.exists("./input"):
+    os.makedirs("./input")
+if not os.path.exists("./input/detection-results"):
+    os.makedirs("./input/detection-results")
+if not os.path.exists("./input/images-optional"):
+    os.makedirs("./input/images-optional")
+
+for image_id in tqdm(image_ids):
+    image_path = "./VOCdevkit/VOC2007/TTEST/Q/"+image_id+".jpg"
+    image = Image.open(image_path)
+    
+    
+    yolo.detect_image(image_id,image)
+    
+print("Conversion completed!")
